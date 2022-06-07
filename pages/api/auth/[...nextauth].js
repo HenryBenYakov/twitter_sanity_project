efe0bf33495cb148721aa9e8219f1d3a318d799e
Henry Ben Yakov <henrybenyakov1@gmail.com>
2022-06-07 14:42:08 +0300

    bug fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,9 +7,9 @@ export default NextAuth({
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
-      secret: process.env.NEXT_PUBLIC_SECRET,
       version: '2.0',
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 })
